c126516ab6c5e14927b5b0021e283c42058722bf
cjthehe <chanjt-pm23@student.tarc.edu.my>
2026-07-16 12:04:41 +0800

    type check: fixed bugs due to dotenv

--- src/database.py
+++ src/database.py
@@ -8,4 +8,7 @@ load_dotenv()
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
+if not url or not key:
+    raise ValueError("Missing Supabase environment variables")
+
 supabase: Client = create_client(url, key)
